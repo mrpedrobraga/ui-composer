dffc58c4b8a3bc2dd9c533ed4b2c1dc92b682e1f
mrpedrobraga <mrhenribraga@gmail.com>
2024-12-24 14:33:13 -0300

    feat: Added flex container example.

--- examples/layout.rs
+++ examples/layout.rs
@@ -1,70 +1,120 @@
 #![allow(non_snake_case)]
+
+use ui_composer::items;
 use ui_composer::prelude::functions::weighted_division_with_minima;
 use ui_composer::prelude::*;
 
 fn main() {
-    let flex = Flex(
+    let flex = Flex(items!(
         FlexItem(
             Square(Rgb::new(255.0 / 255.0, 111.0 / 255.0, 145.0 / 255.0)),
-            1.0,
+            2.0,
         ),
         FlexItem(
             Square(Rgb::new(255.0 / 255.0, 199.0 / 255.0, 95.0 / 255.0)),
+            3.0,
+        ),
+        FlexItem(
+            Square(Rgb::new(255.0 / 255.0, 111.0 / 255.0, 95.0 / 255.0)),
             2.0,
         ),
-    );
+    ));
 
-    App::run(Window(Center(WithSize(Extent2::new(600.0, 300.0), flex))).with_title("Flex Example"))
+    App::run(Window(flex).with_title("Flex Example"))
 }
 
 fn Square(color: Rgb<f32>) -> impl LayoutItem {
-    Resizable::new(move |hx| hx.rect.with_color(color))
-        .with_minimum_size(Extent2::new(100.0, 100.0))
+    Resizable::new(move |hx| {
+        (
+            hx.rect.with_color(color),
+            Rect::new(hx.rect.x, hx.rect.y + 4.0, 200.0, 4.0).with_color(Rgb::white()),
+        )
+    })
+    .with_minimum_size(Extent2::new(200.0, 200.0))
 }
 
 struct FlexItem<T>(T, f32);
 
-struct FlexContainer<A, B> {
-    item_a: FlexItem<A>,
-    item_b: FlexItem<B>,
+struct FlexContainer<TItems: FlexItemCollection> {
+    items: TItems,
 }
 
-fn Flex<A, B>(item_a: FlexItem<A>, item_b: FlexItem<B>) -> FlexContainer<A, B> {
-    FlexContainer { item_a, item_b }
+fn Flex<TItems>(items: TItems) -> FlexContainer<TItems>
+where
+    TItems: FlexItemCollection,
+{
+    FlexContainer { items }
 }
 
-impl<A, B> LayoutItem for FlexContainer<A, B>
+trait FlexItemCollection {
+    type UINodeType: ItemDescriptor;
+
+    fn get_natural_size(&self) -> Extent2<f32>;
+    fn get_minimum_size(&self) -> Extent2<f32>;
+    fn weight(&self) -> f32;
+    fn lay(&mut self, hx: ParentHints) -> Self::UINodeType;
+}
+
+impl<A> FlexItemCollection for FlexItem<A>
 where
     A: LayoutItem,
-    B: LayoutItem,
+{
+    type UINodeType = A::UINodeType;
+
+    fn get_natural_size(&self) -> Extent2<f32> {
+        self.0.get_natural_size()
+    }
+
+    fn get_minimum_size(&self) -> Extent2<f32> {
+        self.0.get_minimum_size()
+    }
+
+    fn weight(&self) -> f32 {
+        self.1
+    }
+
+    fn lay(&mut self, hx: ParentHints) -> Self::UINodeType {
+        self.0.lay(hx)
+    }
+}
+
+impl<A, B> FlexItemCollection for (A, B)
+where
+    A: FlexItemCollection,
+    B: FlexItemCollection,
 {
     type UINodeType = (A::UINodeType, B::UINodeType);
 
     fn get_natural_size(&self) -> Extent2<f32> {
-        let a_size = self.item_a.0.get_natural_size();
-        let b_size = self.item_b.0.get_natural_size();
+        let a_size = self.0.get_natural_size();
+        let b_size = self.1.get_natural_size();
 
         Extent2::new(a_size.w + b_size.w, a_size.h.max(b_size.h))
     }
 
     fn get_minimum_size(&self) -> Extent2<f32> {
-        let a_size = self.item_a.0.get_minimum_size();
-        let b_size = self.item_b.0.get_minimum_size();
+        let a_size = self.0.get_minimum_size();
+        let b_size = self.1.get_minimum_size();
 
         Extent2::new(a_size.w + b_size.w, a_size.h.max(b_size.h))
     }
 
+    fn weight(&self) -> f32 {
+        self.0.weight() + self.1.weight()
+    }
+
     fn lay(&mut self, parent_hints: ParentHints) -> Self::UINodeType {
-        let a_size = self.item_a.0.get_natural_size();
-        let b_size = self.item_b.0.get_natural_size();
+        let a_natural_size = self.0.get_natural_size();
+        let b_natural_size = self.1.get_natural_size();
 
         let sizes = weighted_division_with_minima(
             parent_hints.rect.w,
-            &[self.item_a.1, self.item_b.1],
-            &[a_size.w, b_size.w],
+            &[self.0.weight(), self.1.weight()],
+            &[a_natural_size.w, b_natural_size.w],
+            1.0,
         );
 
-        let a = self.item_a.0.lay(ParentHints {
+        let a = self.0.lay(ParentHints {
             rect: Rect::new(
                 parent_hints.rect.x,
                 parent_hints.rect.y,
@@ -74,7 +124,7 @@ where
             ..parent_hints
         });
 
-        let b = self.item_b.0.lay(ParentHints {
+        let b = self.1.lay(ParentHints {
             rect: Rect::new(
                 sizes[0] + parent_hints.rect.x,
                 parent_hints.rect.y,
@@ -85,5 +135,24 @@ where
         });
 
         (a, b)
+    }
+}
+
+impl<TItems> LayoutItem for FlexContainer<TItems>
+where
+    TItems: FlexItemCollection + Send,
+{
+    type UINodeType = TItems::UINodeType;
+
+    fn get_natural_size(&self) -> Extent2<f32> {
+        self.items.get_natural_size()
+    }
+
+    fn get_minimum_size(&self) -> Extent2<f32> {
+        self.items.get_minimum_size()
+    }
+
+    fn lay(&mut self, parent_hints: ParentHints) -> Self::UINodeType {
+        self.items.lay(parent_hints)
     }
 }
